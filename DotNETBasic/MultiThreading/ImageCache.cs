14048e41eecb978ada67c3369d643d88f01a8e2b
wei <li772555190@live.com>
2016-02-26 18:06:56 +0800

    add  lock

---
+++ DotNETBasic/MultiThreading/ImageCache.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThreading
+{
+	public class ImageCache
+	{
+		private object _syncObject = new object();
+
+		private Dictionary<string, string> _imageDic = new Dictionary<string, string>();
+
+		public string GetImage(string path)
+		{
+			string image = null;
+			Monitor.Enter(_syncObject);
+			image = GetImageInner(path);
+			Monitor.Exit(_syncObject);
+			return image;
+			//lock (_syncObject)
+			//{
+			//return	GetImageInner(path);
+			//}
+		}
+
+		public void AddImage(string path, string image)
+		{
+			lock (_syncObject)
+			{
+				AddImageInner(path, image);
+			}
+		}
+
+
+		public string GetImageInner(string path)
+		{
+			string image = null;
+			if (_imageDic.ContainsKey(path))
+			{
+				image = _imageDic[path];
+			}
+			else
+				image = string.Empty;
+			Console.WriteLine("GetImage path: {0},image: {1} ,threadId: {2}", path, image, Thread.CurrentThread.ManagedThreadId);
+			return image;
+		}
+
+		public void AddImageInner(string path, string image)
+		{
+			_imageDic[path] = image;
+
+			Console.WriteLine("AddImage path: {0},image: {1} ,threadId: {2}", path, image, Thread.CurrentThread.ManagedThreadId);
+		}
+	}
+}
--- DotNETBasic/MultiThreading/Program.cs
+++ DotNETBasic/MultiThreading/Program.cs
@@ -10,6 +10,52 @@ namespace MultiThreading
 	{
 		static void Main(string[] args)
 		{
+			ImageCacheTest();
+			Console.ReadKey();
 		}
+
+
+		public static void ImageCacheTest()
+		{
+			var max = 10;
+			var cache = new ImageCache();
+			Task.Factory.StartNew(() =>
+			{
+				Console.WriteLine("Start Task1");
+				for (int i = 0; i < max; i = i + 2)
+				{
+					cache.AddImage(i.ToString(), (i * 100).ToString());
+				}
+			});
+
+			Task.Factory.StartNew(() =>
+			{
+				Console.WriteLine("Start Task2");
+				for (int i = 0; i < max; i = i + 2)
+				{
+					cache.GetImage(i.ToString());
+				}
+			});
+
+
+			Task.Factory.StartNew(() =>
+			{
+				Console.WriteLine("Start Task3");
+				for (int i = max; i < 2 * max; i = i + 2)
+				{
+					cache.AddImageInner(i.ToString(), (i * 100).ToString());
+				}
+			});
+
+			Task.Factory.StartNew(() =>
+			{
+				Console.WriteLine("Start Task4");
+				for (int i = max; i < 2 * max; i = i + 2)
+				{
+					cache.GetImageInner(i.ToString());
+				}
+			});
+		}
+
 	}
 }
