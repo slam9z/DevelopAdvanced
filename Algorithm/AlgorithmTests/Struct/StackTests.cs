59df91b5582c77bc41acd2082b533e9e602426af
wei772 <li772555190@live.com>
2016-08-21 18:18:06 +0800

    add  stack

---
+++ Algorithm/AlgorithmTests/Struct/StackTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm.Struct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Struct.Tests
+{
+	[TestClass()]
+	public class StackTests
+	{
+		[TestMethod()]
+		public void PushTest()
+		{
+			var stack = new Stack<int>();
+			stack.Push(1);
+			stack.Push(5);
+			stack.Push(3);
+			stack.Push(23);
+			stack.Push(123);
+		}
+
+		[TestMethod()]
+		[ExpectedException(exceptionType: typeof(InvalidOperationException))]
+		public void PopEmptyTest()
+		{
+			var stack = new Stack<int>();
+			stack.Pop();
+
+		}
+
+		[TestMethod()]
+		public void PushAndPopTest()
+		{
+			var stack = new Stack<int>();
+			stack.Push(1);
+			stack.Push(5);
+			stack.Push(3);
+			var pop = stack.Pop();
+			Assert.AreEqual(3, pop);
+
+			pop = stack.Pop();
+			Assert.AreEqual(5, pop);
+
+			Assert.IsFalse(stack.IsEmpty);
+
+			pop = stack.Pop();
+			Assert.AreEqual(1, pop);
+
+			Assert.IsTrue(stack.IsEmpty);
+		}
+
+
+	}
+}
