c131b6aee32549684669867167c89549d5dda864
wei772 <li772555190@live.com>
2016-08-23 18:20:02 +0800

    add   linklist

---
+++ Algorithm/Algorithm/Struct/LinkList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Struct
+{
+	public class LinkList<T> where T : IEquatable<T>
+	{
+		private LinkNode<T> _head;
+
+		public bool IsEmpty
+		{
+			get
+			{
+				return _head == null;
+			}
+		}
+
+		public LinkNode<T> Search(T data)
+		{
+			var node = _head;
+			while (node != null && node.Data.Equals(data))
+			{
+				node = node.Next;
+			}
+			return node;
+		}
+
+		//将新数据插入到最前端
+		public LinkNode<T> Insert(T data)
+		{
+			var newNode = new LinkNode<T>() { Data = data };
+			Insert(newNode);
+			return newNode;
+		}
+
+		public void Insert(LinkNode<T> node)
+		{
+			node.Next = _head;
+			if (_head != null)
+			{
+				_head.Prev = node;
+			}
+			_head = node;
+		}
+
+
+		public void Delete(LinkNode<T> node)
+		{
+			//prev
+			if (node.Prev != null)
+			{
+				node.Prev.Next = node.Next;
+			}
+			else
+			{
+				_head = node.Next;
+			}
+
+			//next
+			if (node.Next != null)
+			{
+				node.Next.Prev = node.Prev;
+			}
+
+		}
+
+
+
+	}
+}
---
+++ Algorithm/AlgorithmTests/Struct/LinkListTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm.Struct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Struct.Tests
+{
+	[TestClass()]
+	public class LinkListTests
+	{
+		[TestMethod()]
+		public void InsertAndDeleteTest()
+		{
+			var link = new LinkList<int>();
+
+			var node1 = link.Insert(1);
+			var node2 = link.Insert(2);
+			var node3 = link.Insert(3);
+			var node4 = link.Insert(4);
+
+			link.Delete(node2);
+
+			//不明觉厉
+			Assert.AreSame(node1.Prev, node3);
+			Assert.AreSame(node3.Next, node1);
+
+			link.Delete(node3);
+			link.Delete(node4);
+			link.Delete(node1);
+
+			Assert.IsTrue(link.IsEmpty);
+
+		}
+	}
+}
