2e4720bb305f81509335b894cfe0fb75343da13f
ucs_liwei <ucs_liwei@LIWEI>
2016-08-30 18:05:05 +0800

    add stringtest project

---
+++ DotNETBasic/StringTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTest
+{
+
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string s)
+        {
+            return String.IsNullOrWhiteSpace(s);
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string key = null;
+            key.IsNullOrWhiteSpace();
+        }
+    }
+}
