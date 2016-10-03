6a064bd02fd91689a5ccae7155d9241253b8d066
wei772 <li772555190@live.com>
2016-10-03 22:10:26 +0800

    update  redblacktree

--- Algorithm/Algorithm/Struct/RedBlackTree.cs
+++ Algorithm/Algorithm/Struct/RedBlackTree.cs
@@ -14,6 +14,97 @@ namespace Algorithm.Struct
 			{
 				Color = NodeColor.Black
 			};
+
+
+		public RedBlackTree()
+		{
+
+		}
+
+
+		#region Rotate  //看着图写步骤，并不是太复杂的逻辑
+
+
+		public void LeftRotate(BinaryTreeNode<T> sourceNode)
+		{
+			var targetNode = sourceNode.Right;
+
+			#region //使targetNode的Left节点变成source的right节点
+
+			sourceNode.Right = targetNode.Left;
+			targetNode.Left.Parent = sourceNode;
+
+			#endregion
+
+
+			#region //使targetNode的变成source的父亲节点
+
+			targetNode.Parent = sourceNode.Parent;
+
+			//漏掉的逻辑,设置parent引用
+			if (sourceNode.Parent == Empty)
+			{
+				Root = targetNode;
+			}
+			else if (sourceNode == sourceNode.Parent.Left)
+			{
+				sourceNode.Parent.Left = targetNode;
+			}
+			else
+			{
+				sourceNode.Parent.Right = targetNode;
+
+			}
+
+			targetNode.Left = sourceNode;
+
+			sourceNode.Parent = targetNode;
+
+
+			#endregion
+
+		}
+
+
+		public void RightRotate(BinaryTreeNode<T> sourceNode)
+		{
+			var targetNode = sourceNode.Parent;
+
+			#region 使source的right节点成为target的left节点
+
+			targetNode.Left = sourceNode.Right;
+			sourceNode.Right.Parent = targetNode;
+
+			#endregion
+
+			#region 使source的成为target的parent node
+
+			sourceNode.Parent = targetNode.Parent;
+
+			//设置parent的引用
+			if (targetNode.Parent == Empty)
+			{
+				Root = sourceNode;
+			}
+			else if (targetNode.Parent.Right == targetNode)
+			{
+				targetNode.Parent.Right = sourceNode;
+			}
+			else
+			{
+				targetNode.Parent.Left = sourceNode;
+			}
+
+			sourceNode.Right = targetNode;
+
+			targetNode.Parent = sourceNode;
+
+			#endregion
+
+		}
+
+
+		#endregion
 	}
 
 
---
+++ Algorithm/AlgorithmTests/Struct/RedBlackTreeTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm.Struct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Struct.Tests
+{
+	[TestClass()]
+	public class RedBlackTreeTests
+	{
+		[TestMethod()]
+		public void RedBlackTreeTest()
+		{
+			Assert.Fail();
+		}
+
+		private void CheckRedBlackTree(RedBlackTree<int> tree)
+		{
+			CheckRoot(tree);
+			CheckRedNode(tree);
+		}
+
+		public void CheckRoot(RedBlackTree<int> tree)
+		{
+			Assert.AreEqual(GetRedBlackTreeNode(tree.Root).Color, NodeColor.Black);
+		}
+
+
+		public void CheckRedNode(RedBlackTree<int> tree)
+		{
+			tree.Inorder(tree.Root, (node) =>
+			 {
+				 var redBlackNode = node as RedBlackTreeNode<int>;
+				 if (redBlackNode.Color == NodeColor.Red)
+				 {
+					 Assert.AreEqual(GetRedBlackTreeNode(redBlackNode.Right).Color, NodeColor.Black);
+					 Assert.AreEqual(GetRedBlackTreeNode(redBlackNode.Left).Color, NodeColor.Black);
+				 }
+			 }
+			);
+		}
+
+
+
+		private RedBlackTreeNode<int> GetRedBlackTreeNode(BinaryTreeNode<int> node)
+		{
+			return node as RedBlackTreeNode<int>;
+		}
+	}
+}
