035e45c34b58b65b4f26f34950c3923c27c60a26
wei772 <li772555190@live.com>
2016-10-08 21:11:00 +0800

    add  AVLTree

---
+++ Algorithm/Algorithm/Struct/AVLTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Struct
+{
+	// AVL tree (Georgy Adelson-Velsky and Evgenii Landis' tree, named after the inventors)
+
+	public class AVLTree<T> : BinarySearchTree<T> where T : IComparable
+	{
+
+	}
+}
