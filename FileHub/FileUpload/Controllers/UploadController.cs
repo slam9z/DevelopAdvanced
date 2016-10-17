c5f553caad3cc30a4b685a57fec11d9bc9db642c
ucs_liwei <ucs_liwei@LIWEI>
2016-10-17 16:55:03 +0800

    init  filehub

---
+++ FileHub/FileUpload/Controllers/UploadController.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+public class UploadController : ApiController
+{
+    public async Task<HttpResponseMessage> PostFile()
+    {
+        // Check if the request contains multipart/form-data.
+        if (!Request.Content.IsMimeMultipartContent())
+        {
+            throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+        }
+
+        string root = HttpContext.Current.Server.MapPath("~/App_Data");
+        var provider = new MultipartFormDataStreamProvider(root);
+
+        try
+        {
+
+            dynamic result = new JObject();
+            // Read the form data and return an async task.
+            await Request.Content.ReadAsMultipartAsync(provider);
+
+            // This illustrates how to get the form data.
+            foreach (var key in provider.FormData.AllKeys)
+            {
+                foreach (var val in provider.FormData.GetValues(key))
+                {
+
+                }
+            }
+
+            var list = new JArray();
+            // This illustrates how to get the file names for uploaded files.
+            foreach (var file in provider.FileData)
+            {
+                FileInfo fileInfo = new FileInfo(file.LocalFileName);
+                dynamic fileJson = new JObject();
+                fileJson.name = fileInfo.Name;
+                list.Add(fileJson);
+            }
+            result.files = list;
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(result.ToString())
+            };
+
+
+        }
+        catch (System.Exception e)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+        }
+    }
+
+}
