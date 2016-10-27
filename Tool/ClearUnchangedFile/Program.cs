f6abba1f091c911933e3f2dfb6329e984ce04a20
ucs_liwei <ucs_liwei@LIWEI>
2016-10-27 15:31:22 +0800

    add  ClearUnchangedFile  tool

---
+++ Tool/ClearUnchangedFile/Program.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearUnchangedFile
+{
+    class Program
+    {
+
+        private static Setting _settings = new Setting();
+
+        private static Storage _storage = new Storage();
+
+        /// <summary>
+        ///
+        /// 
+        /// </summary>
+        /// <param name="compareSourceFolder"></param>
+        /// <param name="compareTargetFolder"></param>
+        /// <param name="forceDeleteFiles"></param>
+        public static void SyncFiles(
+                StorageFolderInfo clearFolder,
+                StorageFolderInfo compareFolder,
+                IList<string> forceDeleteFiles
+                )
+        {
+            var compareFiles = _storage.GetAllFiles(compareFolder);
+            var changeFileCount = 0;
+
+            _storage.Traverse(clearFolder,
+            (folder, file) =>
+            {
+                if (forceDeleteFiles.Contains(file.Name))
+                {
+                    Console.WriteLine($"force delete file {file.RelativePath}");
+                    File.Delete(file.FullPath);
+                }
+                var hasSameFile = compareFiles
+                                    .Any((cmpFile) =>
+                                        cmpFile.RelativePath == file.RelativePath
+                                        && cmpFile.LastWriteTime == file.LastWriteTime
+                                    );
+
+                //删除clearFolder里与compareFolder相同的文件
+                if (hasSameFile)
+                {
+                    File.Delete(file.FullPath);
+                    folder.Files.Remove(file);
+                }
+                else
+                {
+                    changeFileCount++;
+                    Console.WriteLine($"change file {file.RelativePath}");
+
+                    //将不相同的文件复制到compareFolder
+                    var newPath = compareFolder.RootFolder + file.RelativePath;
+                    File.Copy(file.FullPath, newPath, true);
+                }
+            }
+            );
+            Console.WriteLine($"total change file {changeFileCount}");
+
+            //删除空文件夹
+            _storage.Traverse(clearFolder,
+                folderAction: (folder) =>
+                {
+                    if (
+                    folder.Parent != null
+                    && folder.Files.Count == 0
+                    && folder.ChildFolders.Count == 0)
+                    {
+                        Directory.Delete(folder.FullPath);
+                        folder.Parent.ChildFolders.Remove(folder);
+                    }
+                }
+                );
+        }
+
+
+        static void Main(string[] args)
+        {
+            var setting = _settings.GetSettingInfo();
+            var clearFolder = _storage.CreateFolder(setting.ClearFolder);
+            var compareFolder = _storage.CreateFolder(setting.CompareFolder);
+
+            if (compareFolder == null ||
+                (compareFolder.Files.Count == 0 && compareFolder.ChildFolders.Count == 0))
+            {
+                Console.WriteLine($"copying folder  {setting.ClearFolder} to {setting.CompareFolder}");
+
+                _storage.CopyFolder(setting.ClearFolder, setting.CompareFolder);
+
+                Console.WriteLine("copy folder finished");
+                Console.ReadKey();
+
+            }
+            else
+            {
+                Console.WriteLine("clear unchanged file");
+                Console.WriteLine();
+
+                SyncFiles(clearFolder, compareFolder, setting.ForceDeleteFiles);
+
+                Console.WriteLine();
+                Console.WriteLine("clear unchanged file finished");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Tool/ClearUnchangedFile/Setting.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClearUnchangedFile
+{
+    public class SettingInfo
+    {
+        public string ClearFolder { get; set; }
+        public string CompareFolder { get; set; }
+        public IList<string> ForceDeleteFiles { get; set; }
+    }
+
+    public class Setting
+    {
+        public string ClearFolder { get; set; }
+        public string CompareFolder { get; set; }
+        public IList<string> ForceDeleteFiles { get; set; }
+
+
+        public SettingInfo GetSettingInfo()
+        {
+            return LoadSetting(SelectSettingFile(GetSettingFiles()));
+        }
+        public IList<string> GetSettingFiles()
+        {
+            var current = new DirectoryInfo(Directory.GetCurrentDirectory());
+            return current.GetFiles("*setting.json").Select(o => o.Name).ToList();
+        }
+        public string SelectSettingFile(IList<string> files)
+        {
+            var count = files.Count;
+            if (count == 0)
+            {
+                return null;
+            }
+            if (count == 1)
+            {
+                return files[0];
+            }
+
+            Console.WriteLine("all setting");
+            Console.WriteLine();
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.WriteLine();
+            Console.WriteLine("select setting file , can user regexp");
+
+            while (true)
+            {
+                var select = Console.ReadLine();
+                if (select.Length < 3)
+                {
+                    Console.WriteLine("select key length must greater than 2");
+                    continue;
+                }
+
+                foreach (var file in files)
+                {
+                    if (file == select)
+                    {
+                        return file;
+                    }
+                }
+
+                var regExp = new Regex(select);
+                foreach (var file in files)
+                {
+                    if (regExp.IsMatch(file))
+                    {
+                        return file;
+                    }
+                }
+            }
+        }
+
+        public SettingInfo LoadSetting(string settingFileName)
+        {
+            Console.WriteLine($"load setting {settingFileName}");
+
+            var json = File.ReadAllText(settingFileName);
+            return (SettingInfo)JsonConvert.DeserializeObject(json, typeof(SettingInfo));
+        }
+    }
+
+
+
+}
