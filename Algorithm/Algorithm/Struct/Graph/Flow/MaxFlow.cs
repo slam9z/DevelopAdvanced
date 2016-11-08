1a49f3f594ec77cd7517e1f625c79c8548702f2b
wei772 <li772555190@live.com>
2016-11-09 00:36:27 +0800

    add  EdmondsKarp

---
+++ Algorithm/Algorithm/Struct/Graph/Flow/MaxFlow.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Struct
+{
+    public class MaxFlow<T> where T : IEquatable<T>
+    {
+        /// <summary>
+        /// 看不懂算法导论上的实现
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        public int EdmondsKarp(
+            AdjacencyListGraph<T> graph
+            , AdjacencyVertex<T> source
+            , AdjacencyVertex<T> target)
+        {
+
+            var maxflow = 0;
+
+            var edges = graph.GetEdges().Select(d => d as FlowEdge<T>);
+
+            foreach (var edge in edges)
+            {
+                edge.Flow = 0;
+            }
+
+
+            while (true)
+            {
+                var path = GetResidualPath(graph, source, target);
+                if (path == null || path.Count == 0)
+                {
+                    break;
+                }
+                var tempFlow = int.MaxValue;
+
+                //感觉第一次写这样的for循环
+                for (var edge = path[target]; edge != null; edge = path[edge.Start])
+                {
+                    tempFlow = Math.Min(tempFlow, edge.ResidualCapacity);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine($"pathflow:{tempFlow}");
+
+                for (var edge = path[target]; edge != null; edge = path[edge.Start])
+                {
+                    edge.Flow = edge.Flow + tempFlow;
+                    edge.Revolution.Flow = edge.Revolution.Flow - tempFlow;
+                    Console.WriteLine($"edge:{edge}");
+                }
+
+                maxflow = maxflow + tempFlow;
+            }
+
+            return maxflow;
+        }
+
+        public IDictionary<AdjacencyVertex<T>, FlowEdge<T>> GetResidualPath(
+            AdjacencyListGraph<T> graph
+            , AdjacencyVertex<T> source
+            , AdjacencyVertex<T> target)
+        {
+
+            var path = new Dictionary<AdjacencyVertex<T>, FlowEdge<T>>();
+
+            var vertexs = graph.GetVertexs();
+            foreach (var vertex in vertexs)
+            {
+                vertex.Predecessor = null;
+            }
+
+            path[source] = null;
+
+            var queue = new Queue<AdjacencyVertex<T>>();
+            queue.Enqueue(source);
+
+            while (!queue.IsEmpty)
+            {
+                var current = queue.Dequeue();
+                var currentEdges = graph.GetVertexEdge(current);
+                foreach (FlowEdge<T> currentEdge in currentEdges)
+                {
+                    if (currentEdge.End != source
+                        && currentEdge.End.Predecessor == null
+                        && currentEdge.Capacity>currentEdge.Flow
+                        )
+                    {
+                        path[currentEdge.End] = currentEdge;
+                        currentEdge.End.Predecessor = current;
+                        queue.Enqueue(currentEdge.End);
+                    }
+                }
+            }
+
+            if (target.Predecessor == null)
+            {
+                return null;
+            }
+
+            return path;
+        }
+    }
+}
