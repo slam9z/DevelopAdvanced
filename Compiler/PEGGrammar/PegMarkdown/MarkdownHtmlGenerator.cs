4ea008b8b1f96eb6699314a2d47aa2072214fbd6
ucs_liwei <ucs_liwei@LIWEI>
2016-12-11 23:30:43 +0800

    add  markdown2html

---
+++ Compiler/PEGGrammar/PegMarkdown/MarkdownHtmlGenerator.cs
@@ -0,0 +1,46 @@
+﻿using Peg.Generator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peg.Markdown
+{
+    enum MarkdownKind
+    {
+        Heading,
+    }
+
+
+    struct CodeTemplate
+    {
+        internal CodeTemplate(MarkdownKind kind, string codeTemplate)
+        {
+            Kind = kind;
+            sCodeTemplate = codeTemplate;
+        }
+        internal MarkdownKind Kind;
+        internal string sCodeTemplate;
+    }
+
+
+    public class MarkdownHtmlGenerator
+    {
+        private TreeContext _context;
+
+
+        CodeTemplate[] templates = {
+            new CodeTemplate( MarkdownKind.Heading,@"<h${HeaderLevel}>&{Html}<h${HeaderLevel}>"),
+        };
+
+        public MarkdownHtmlGenerator(TreeContext context)
+        {
+
+
+
+        }
+
+ 
+    }
+}
